e8b72e49d7e9d68810b83c78b244b8c0629f70ad
kdeoa0 <115581174+kdeoa0@users.noreply.github.com>
2023-04-24 17:46:00 +1000

    Add files via upload

--- java/main.js
+++ java/main.js
@@ -11,12 +11,20 @@ function reloj() {
 }
 setInterval(reloj, 1000);
 
+function contacto(){
+    var sapo = document.getElementById("nombre");
+    var sapa = document.getElementById("mensaje");
+    //nobody is going to read the this!!!
+    sapo.value="";
+    sapa.value="";
+    alert("enviado")
+}
 
 
 
 
-
-
+var titulox=[];
+var cuerpox=[];
 
 let contador=0;
 function vida(){
@@ -27,53 +35,71 @@ var pepo = document.getElementById("articuloo").value;
 var grid = document.getElementById("gridepica");
 var elemento = document.createElement("div");
 var elemento2 = document.createElement("div");
-var elemento3 = document.createElement("div");
+var elemento3 = document.createElement("p");
+var nuevo = document.createElement("p");
+
+//clase epica
+elemento.className="grid-item-2";
+
+//variables para borrar
+nuevo.id ="nuevo";
+nuevo.className="nuevo";
+elemento3.className="nuevo";
+elemento3.id="nueva";
+elemento2.className="cuerpo";
+
+
 elemento2.style.gridRow = 2;
+//if para borrar
+var deletee= document.getElementById("nuevo");
+var deleteee= document.getElementById("nueva");
+if (contador>1){
+    deletee.parentNode.removeChild(deletee);
+    deleteee.parentNode.removeChild(deleteee);
+
+}
 
 elemento.textContent="Titulo: "+pepi;
 elemento2.textContent="Cuerpo: "+pepo;
-elemento3.textContent="<!--  -->";
+//para peticion de articulos
+
+
+
+elemento3.textContent="ARTICULO MAS RECIENTE";
+nuevo.textContent="ARTICULO MAS RECIENTE";
 elemento.style.gridRow = "1";
 elemento2.style.gridRow = "2";
 elemento3.style.gridRow = "3";
+
+grid.appendChild(nuevo);
 grid.appendChild(elemento);
 grid.appendChild(elemento2);
 grid.appendChild(elemento3);
+
 document.getElementById("perron").innerHTML = conta;
 document.getElementById("titulo").value="";
 document.getElementById("articuloo").value="";
 
+titulox.push(pepi);
+cuerpox.push(pepo);
 }
 
 
 
-let caja=[];
-
-var listado = document.getElementById('listado');
-var item = document.createElement('li');
-item.appendChild(document.createTextNode(pepi));
-listado.appendChild(item);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function contacto(){
-    var sapo = document.getElementById("nombre");
-    var sapa = document.getElementById("mensaje");
-    //nobody is going to read the this!!!
-    sapo.value="";
-    sapa.value="";
-    alert("enviado")
+//mini articulo
+function miniarti(){
+    var numbere= document.getElementById("papitas").value;
+    var josuke= titulox[parseInt(numbere)];
+    var joline= cuerpox[parseInt(numbere)];
+    var jotaro= "Titulo: "+josuke+" <br> Cuerpo: "+joline;
+    document.getElementById("old-article").innerHTML =jotaro;
+    console.log("papitas: " + jotaro);
 }
+
+
+
+
+
+
+
+

5a905544a594e437ec37fb54c644df379bdc2f17
kdeoa0 <115581174+kdeoa0@users.noreply.github.com>
2023-05-17 02:14:02 +1000

    Add files via upload

--- Moldelo/database.sql
+++ Moldelo/database.sql
@@ -1,18 +1,42 @@
 
-create database if not exists Elfaro;
-use Elfaro;
-create table Articulos(
+create database if not exists elfaro;
+use elfaro;
+create table if not exists Articulos(
+id int auto_increment primary key,	
  Titulo	varchar(50),
  Cuerpo longtext
 );
-create table Clientservice(
+create table if not exists Clientservice(
+	id int auto_increment primary key,	
 	Username varchar(255),
     peticion longtext
 );
+create table if not exists users(
+	id int auto_increment primary key,	
+	usersname varchar(255),
+    clave varchar(255)
+);
 
-insert into Articulos (Titulo, Cuerpo)
-VALUES ("xxx", "ddd");
+	DELIMITER //
 
-insert into Clientservice (Username, peticion)
-VALUES ("yyy", "eee");
-/*no coneccion for the moment*/
+	CREATE PROCEDURE SelectData()
+	BEGIN
+		SELECT usersname FROM users;
+	END //
+
+	DELIMITER ;
+    
+    	DELIMITER //
+
+	CREATE PROCEDURE InsertDataUsers(IN p_username VARCHAR(255), IN p_password VARCHAR(255))
+	BEGIN
+		INSERT INTO users (usersname, clave) VALUES (p_username, p_password);
+	END //
+
+	DELIMITER ;
+
+ -- select * from users;
+
+-- INSERT INTO users(usersname, clave) VALUES ('$name', '$password');
+
+ -- drop table users;	

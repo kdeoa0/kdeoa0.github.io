a204ba5774d1331eb5d395456030b8f2dad64994
kdeoa0 <115581174+kdeoa0@users.noreply.github.com>
2023-05-23 20:18:47 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -23,16 +23,20 @@
     <div style = "position:absolute; right:0px; top:0px;">
     <img src="Vista/images/MainLogo.png" width="100" >
     </div>
-    <hr>
-    <h2>
-        Avisos: !!!!!! Follow this channel : <a href="https://www.twitch.tv/nihmune">https://www.twitch.tv/nihmune</a> 
-    </h2>
-    <hr>
+    <!--Links--> 
     <h2>    
         <a href="index.php">Inicio</a> 
         <br><a href="sport.php">Deporte</a>
         <br><a href="negocio.php">Negocios</a>
     </h2>
+    <!--Seccion avisos--> 
+    <hr>
+    <h1 class="Aviso">Sección de Avisos</h1>
+    <div class="Saviso">
+    <h2 >Aviso!!!!!!  Follow this channel : 
+    <p><a href="https://www.twitch.tv/nihmune">https://www.twitch.tv/nihmune</a></p> 
+    </h2>
+    </div>
     <hr>
     <h1 id="noticiass">NOTICIAS</h1>
 
@@ -40,46 +44,48 @@
     <div class="grid-item grid-item-3" id="gridepica"></div>
 
         <!--formulario1-->
+    <form class="formulario">
     <h1 id="formu">Formulario articulo</h1>
-
-    <label for="titulo">Titulo</label>
+    <label for="titulo">Titulo:</label><br>
     <input type="text" id="titulo"> <br>
-    <label for="articuloo">Cuerpo</label>
+    <label for="articuloo">Cuerpo:</label><br>
     <input type="text" id="articuloo">
     <br>
     <input type="button" id="boton4" onclick="vida()"  value="Press"> 
-
+    </form>
 
     <!--formulario2-->
-    <br>
+    <br><br>
+    <form class="formulario">
     <h1 id="formus">Formulario contacto</h1>
-    <label for="nombre">Nombre</label>
-    
-
+    <label for="nombre">Nombre:</label><br>
     <input type="text" id="nombre"> <br>
-    <label for="mensaje">Mensaje</label>
-    <input type="text" id="mensaje">
-
-    <input type="button" id="boton2" value="Press" onclick="contacto()"> 
+    <label for="mensaje">Mensaje:</label><br>
+    <input type="text" id="mensaje"><br>
+    <input type="button" id="boton2" value="Press" onclick="contacto()"> <br>
+    </form>
     <a href="sport.php">click para recargar la pagina</a>
-            <!--formulario3-->
-    <h1 id="formu3">Ingreso usuario</h1>
 
-    <form method="post">
-        <label>Name:</label><br>
+    <!--formulario3-->
+    <br><br>
+    <form method="post" class="formulario">
+        <h1 id="formu3">Ingreso usuario</h1>
+        <label>Nombre:</label><br>
 	    <input type="text" name="name"><br>
-	    <label>Password:</label><br>
+	    <label>Contraseña:</label><br>
         <input type="password" name="password"><br>
-	    <input type="submit" name="submit" value="submit" onclick="Login()">
+	    <input type="submit" name="submit" value="Press" onclick="Login()">
     </form>
 
     <br>
     <!--articulo anterior-->
+    
     <h1 id="formu4">Articulo anterior</h1>
     <div id="old-article"> </div>
     <label for="papitas">Elige el numero de tu articulo:</label> <br>
     <input type="number" id="papitas" min="0" >
     <input type="button" id="miniarti" onclick="miniarti()" value="press me">
+    
     <br><br><br>
     <video height="540" autoplay controls>
         <source src="Vista/video/rickroll.mp4" type="video/mp4">
